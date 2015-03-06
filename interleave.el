27888158fd87523db7354e8c7ef434264a032827
Sebastian Christ <rudolfo.christ@gmail.com>
2015-03-06 09:37:53 +0100

    Add some more documentation.

--- interleave.el
+++ interleave.el
@@ -21,9 +21,24 @@
 
 ;;; Commentary:
 ;;
-;; After setting the #+INTERLEAVE_PDF property in the header section of you org file to the PDF file
-;; you'd like to use you can run M-x interleave RET to start up interleave mode. This will
-;; display your PDF and notes side by side.
+;; In the past, textbooks were sometimes published as /interleaved/ editions. That meant, each page
+;; was followed by a blank page and the ambitious student/scholar had the ability to take his notes directly
+;; in her copy of the textbook. Newton and Kant were prominent representatives of this technique.
+
+;; Nowadays textbooks (or lecture material) come in PDF format. Although almost every PDF has the ability to add some notes to the PDF itself, it is not as powerful as it could be.
+;; This is what this minor mode tries to accomplish. It presents your PDF side by side to an [[http://orgmode.org][Org Mode]] buffer with you notes.
+;; Narrowing down to just those passages that are relevant to this particular page in the document viewer.
+
+;;; Usage:
+;;
+;; Create a Org file thath will keep your notes. In the Org headers section add
+;; #+INTERLEAVE_PDF: /the/path/to/your/pdf.pdf
+;;
+;; Then start 'interleave' with
+;; M-x interleave
+;;
+;; To insert a note for a page, type i.
+;; Navigation is the same as in `doc-view-mode'.
 
 ;;; Code:
 
@@ -133,7 +148,26 @@ jump to the notes buffer."
 
 ;;;###autoload
 (define-minor-mode interleave
-  "Interleaving your text books since 2015."
+  "Interleaving your text books since 2015.
+
+In the past, textbooks were sometimes published as /interleaved/ editions. That meant, each page
+was followed by a blank page and the ambitious student/scholar had the ability to take his notes directly
+in her copy of the textbook. Newton and Kant were prominent representatives of this technique.
+
+Nowadays textbooks (or lecture material) come in PDF format. Although almost every PDF has the ability to add some notes to the PDF itself, it is not as powerful as it could be.
+This is what this minor mode tries to accomplish. It presents your PDF side by side to an [[http://orgmode.org][Org Mode]] buffer with you notes.
+Narrowing down to just those passages that are relevant to this particular page in the document viewer.
+
+Usage:
+
+Create a Org file thath will keep your notes. In the Org headers section add
+#+INTERLEAVE_PDF: /the/path/to/your/pdf.pdf
+
+Then start 'interleave' with
+M-x interleave
+
+To insert a note for a page, type 'i'.
+Navigation is the same as in `doc-view-mode'."
   :lighter " Interleave"
   (when interleave
     (setq *interleave--org-buf* (current-buffer))
