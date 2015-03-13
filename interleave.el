987f87537ac41da331f2c28c1916c1f2d9791e66
Sebastian Christ <rudolfo.christ@gmail.com>
2015-03-13 15:02:18 +0100

    A word is a word is a word

--- interleave.el
+++ interleave.el
@@ -25,7 +25,7 @@
 ;; was followed by a blank page and the ambitious student/scholar had the ability to take his notes directly
 ;; in her copy of the textbook. Newton and Kant were prominent representatives of this technique.
 
-;; Nowadays textbooks (or lecture material) come in PDF format. Although almost every PDF has the ability to add some notes to the PDF itself, it is not as powerful as it could be.
+;; Nowadays textbooks (or lecture material) come in PDF format. Although almost every PDF Reader has the ability to add some notes to the PDF itself, it is not as powerful as it could be.
 ;; This is what this minor mode tries to accomplish. It presents your PDF side by side to an [[http://orgmode.org][Org Mode]] buffer with you notes.
 ;; Narrowing down to just those passages that are relevant to this particular page in the document viewer.
 
@@ -154,7 +154,7 @@ In the past, textbooks were sometimes published as /interleaved/ editions. That 
 was followed by a blank page and the ambitious student/scholar had the ability to take his notes directly
 in her copy of the textbook. Newton and Kant were prominent representatives of this technique.
 
-Nowadays textbooks (or lecture material) come in PDF format. Although almost every PDF has the ability to add some notes to the PDF itself, it is not as powerful as it could be.
+Nowadays textbooks (or lecture material) come in PDF format. Although almost every PDF Reader has the ability to add some notes to the PDF itself, it is not as powerful as it could be.
 This is what this minor mode tries to accomplish. It presents your PDF side by side to an [[http://orgmode.org][Org Mode]] buffer with you notes.
 Narrowing down to just those passages that are relevant to this particular page in the document viewer.
 
