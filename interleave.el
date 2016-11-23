f1dafc032ed4cd4a71207b115a5ed9ec868b12e6
Sebastian Christ <rudolfo.christ@gmail.com>
2016-11-23 09:59:44 +0100

    Bump v1.4.0

--- interleave.el
+++ interleave.el
@@ -2,7 +2,7 @@
 
 ;; Author: Sebastian Christ <rudolfo.christ@gmail.com>
 ;; URL: https://github.com/rudolfochrist/interleave
-;; Version: 1.3.20161108-492
+;; Version: 1.4.0
 
 ;; This file is not part of GNU Emacs
 
